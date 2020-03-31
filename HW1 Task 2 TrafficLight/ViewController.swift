b6f137fd0d137399d4c7633809ee68e0e8e2b3d8
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-03-31 08:41:28 +0300

    Ïnitial commit

---
+++ HW1 Task 2 TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1 Task 2 TrafficLight
+//
+//  Created by Ivan Prybolovetz on 3/31/20.
+//  Copyright © 2020 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
