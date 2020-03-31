4e7816c42da9ef352bd0fbf289cb92c5e323f6bc
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-03-31 09:35:34 +0300

    Added Lebel

--- HW1 Task 2 TrafficLight/ViewController.swift
+++ HW1 Task 2 TrafficLight/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
