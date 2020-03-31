60646edb609307624b4ca95389097ba3bc9b9064
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-03-31 11:34:00 +0300

    Add Constraint Button"Stast" , Labels

--- HW1 Task 2 TrafficLight/ViewController.swift
+++ HW1 Task 2 TrafficLight/ViewController.swift
@@ -20,18 +20,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     
+    private var currentLight = CurrentLight.red
+       
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         startButton.layer.cornerRadius = 25
         
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLight.layer.cornerRadius = redLight.frame.height / 2
+        greenLight.layer.cornerRadius = redLight.frame.height / 2
     }
 
     @IBAction func startButtonPressed() {
     
         startButton.setTitle("NEXT", for: .normal)
         
-        
+        switch currentLight {
+            case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+            case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+            case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
     }
-    
 }
-
